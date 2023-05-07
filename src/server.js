44968ee149deb8f32c7c89515e328c4cc33cdb7d
ChauVT2 <vantrungchau2k@gmail.com>
2023-05-07 10:27:43 +0700

    test

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express()// create app for express
 require('dotenv').config()
 const port = process.env.port || 8888
 const hostname = process.env.hostname
+const mysql = require('mysql2');// get the client
 
 
 //config static file and viewEngine
@@ -12,6 +13,25 @@ configViewEngine(app)
 //define route
 app.use('/', webRouters) ///test tien o dung truoc cac routes
 
+// test connection 
+// create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.db_host,
+    port: process.env.db_port,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name
+});
+//end connection database
+
+// simple query
+connection.query(
+    'SELECT * FROM Users ',
+    function (err, results, fields) {
+        console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 app.listen(port, hostname, () => {
